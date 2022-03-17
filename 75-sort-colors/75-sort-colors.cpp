2f55fa8b763b9e51167c2d0131ecb2b2c36e614f
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-03-17 13:29:44 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.63%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,high=nums.size()-1,mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){swap(nums[low],nums[mid]);low++;mid++;}
+            else if(nums[mid]==1)mid++;
+            else if(nums[mid]==2){swap(nums[mid],nums[high]);high--;}
+        }
+        // int a0=0,a1=0,a2=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0)a0++;
+        //     if(nums[i]==1)a1++;                                                                             if(nums[i]==2)a2++;
+        // }
+        // for(int i=0;i<a0;i++)nums[i]=0;
+        // for(int i=a0;i<a1+a0;i++)nums[i]=1;
+        // for(int i=a0+a1;i<nums.size();i++)nums[i]=2;
+    }
+};
