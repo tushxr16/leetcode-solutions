b5ce61a07e40ce7ed0ea46bfa7a552b84bc538a5
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-03-18 13:49:52 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+ // } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        Node *mid=head,*tail=head;
+        //Mid Pointer find kar rha hu
+        while(tail->next!=nullptr && tail->next->next!=nullptr){
+            tail=tail->next->next;
+            mid=mid->next;
+        }
+        //Reverse karunga ab hihi
+        Node *prev=NULL,*curr=mid;
+        while(curr!=nullptr){
+            Node *tmp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=tmp;
+        }
+        while(prev!=nullptr && head!=nullptr){
+            if(prev->data!=head->data)return false;
+            prev=prev->next;
+            head=head->next;
+        }
+        return true;
+    }
+};
+
+
+
+// { Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
