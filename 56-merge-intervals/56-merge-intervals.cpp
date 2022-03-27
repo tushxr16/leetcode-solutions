3ad1d2045398e0559dea95ad952a5e4e25e2f58e
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-03-28 01:35:20 +0530

    Time: 61 ms (36.43%), Space: 18.9 MB (85.17%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for(int i=1;i<intervals.size();i++){
+            if(res.back()[1]<intervals[i][0]){
+                res.push_back(intervals[i]);
+            }else{
+                res.back()[1] = max(res.back()[1],intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
