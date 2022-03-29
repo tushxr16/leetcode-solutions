e2872e27d9c343f578dcc0dcde5241129df5f818
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-03-30 01:53:46 +0530

    Time: 96 ms (96.46%), Space: 61.2 MB (61.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         bitset<100001> b;
-        for(int i=0;i<nums.size();i++){
-            if(b[nums[i]]!=0)return nums[i];
-            else b[nums[i]]=1;
+        for(auto x:nums){
+            if(b[x]!=0)return x;
+            else b[x]=1;
         }
         return -1;
     }
