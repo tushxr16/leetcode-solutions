9e6652993ce4d79d90a9df93288d1a09f7b60ff2
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-01 11:59:46 +0530

    Time: 28 ms (98.70%), Space: 27.6 MB (63.58%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0,high=nums.size(),mid;
+        while(low<high){
+            mid=(low+high)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)high--;
+            else low++;
+        }
+        return -1;
+    }
+};
