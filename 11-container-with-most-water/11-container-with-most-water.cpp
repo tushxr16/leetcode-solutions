7edda932343b1121bc7d94d3fa6a3660794aa75b
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-05 19:46:01 +0530

    Time: 170 ms (14.93%), Space: 59 MB (38.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -3,7 +3,7 @@ public:
     int maxArea(vector<int>& height) {
         int ans=0,left=0,right=height.size()-1;
         while(left<right){
-            ans = max(ans,(right-left)*min(height[left],height[right]));
+            ans = max(ans,(right-left)*(height[left]<height[right]?height[left]:height[right]));
             height[left]>height[right]?right--:left++;
         }
         return ans;
