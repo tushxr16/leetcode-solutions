72e8d31a5c504217672f7511a8b57ff0099d0b5a
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-15 02:30:34 +0530

    Time: 266 ms (63.62%), Space: 81.2 MB (18.38%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(int i=0;i<arr.size();i++){
             if(mp.find(arr[i])!=mp.end())mp[arr[i]]++;
             else mp[arr[i]]=1;
