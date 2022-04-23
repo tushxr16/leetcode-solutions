3c5f35c131fb19b4ab911effd91a7359fd3f5a76
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-24 02:08:12 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (66.14%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    map<string,string> mp;
+    vector<string> v;
     int i=0;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         string s = "http://tinurl.com/"+to_string(i);
-        mp.insert({s,longUrl});
+        v.push_back(longUrl);
         i++;
         return s;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return mp[shortUrl];
+        string num;
+        for(int i=shortUrl.size();i>-1;i--)
+            if(shortUrl[i]!='/')
+                num.push_back(shortUrl[i]);
+            else break;
+        for(int i=0;i<num.size()/2;i++)swap(num[i],num[num.size()-1-i]);
+        return v[stoi(num)];
     }
 };
 
