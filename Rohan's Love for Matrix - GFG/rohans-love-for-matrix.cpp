55bd3672fec9468115a65d2b7e78c7a46295e6ba
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-05-16 12:19:40 +0530

    Added solution - LeetHub

--- Rohan's Love for Matrix - GFG/rohans-love-for-matrix.cpp
+++ Rohan's Love for Matrix - GFG/rohans-love-for-matrix.cpp
@@ -9,12 +9,12 @@ class Solution {
   public:
     int firstElement(int n) 
     {
-        int dp[n+1]={0};
-        dp[0]=dp[1]=dp[2]=1;
-        for(int i=3;i<n+1;i++){
-            dp[i]=(dp[i-1]%1000000007+dp[i-2]%1000000007)%1000000007;
+        int dp[n]={0};
+        dp[0]=dp[1]=1;
+        for(int i=2;i<n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
         }
-        return dp[n];
+        return dp[n-1];
     }
 };
 
