a9c3447222fdfcadddd4e4b99d5883b78b144d92
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-05-17 13:04:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (67.50%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size())return false;
-        return (s+s).find(goal) != -1;
+        int s_size=s.size(),goal_size=goal.size();
+        if(s_size!=goal_size)return false;
+        vector<int> indexes;
+        for(int i=0;i<goal_size;i++)
+            if(s[0]==goal[i])
+                indexes.push_back(i);
+        if(indexes.size()==0)return false;
+        for(int i=0;i<indexes.size();i++){
+            int n=0;
+            for(int j=0;j<goal_size;j++){
+                if(goal[(j+indexes[i])%goal_size]==s[j])
+                    n++;
+            }
+            if(n==goal_size)return true;
+        }
+        return false;
     }
 };
