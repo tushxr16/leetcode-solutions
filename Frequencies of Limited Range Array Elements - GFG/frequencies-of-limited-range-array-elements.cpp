48a34890887a294753df7ceed058cd72bb38c4f5
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-06-04 17:15:31 +0530

    Added solution - LeetHub

--- Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.cpp
+++ Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.cpp
@@ -8,11 +8,13 @@ class Solution{
     //Function to count the frequency of all elements from 1 to N in the array.
     void frequencyCount(vector<int>& arr,int N, int P)
     {
-        int a[max(N,P)]={0};
         for(int i=0;i<N;i++)
-            a[arr[i]-1]++;
+            arr[i]--;
         for(int i=0;i<N;i++)
-            arr[i]=a[i];
+            if(arr[i]%P<N)
+                arr[arr[i]%P] += P;
+        for(int i=0;i<N;i++)
+            arr[i]/=P;
         return;
     }
 };
