2d456dfead7285e6902a8b04976468ee620d88a7
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-06-10 03:15:38 +0530

    Time: 3 ms (45.12%), Space: 6.3 MB (52.34%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int n =s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }else{
+                if(st.size()>0){
+                    if(st.top()=='('){
+                        st.pop();
+                    }else st.push(s[i]);
+                }else st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
