accd186c48cb0be5b2cdfdb8d90c16b2826e5707
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-06-27 19:04:41 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -9,23 +9,7 @@ using namespace std;
 class Solution{
   public:
     int minJumps(int arr[], int n){
-        int maxJump = arr[0], step = arr[0], jump = 1;
-        if(n<=1)
-            return 0;
-        if(arr[0]==0)
-            return -1;
-        for(int i=1;i<n;i++){
-           if(i==n-1)
-            return jump;
-            maxJump = max(maxJump,i + arr[i]);
-            step--;
-            if(step==0){
-                jump++;
-                if(i>=maxJump)
-                    return -1;
-                step = maxJump-i;
-            }
-        }
+        i
         return -1;
     }
 };
