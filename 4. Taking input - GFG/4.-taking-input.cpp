6847d2cb8385ac49db86fda99e880c6c27467e1e
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-06-30 03:48:08 +0530

    Added solution - LeetHub

---
+++ 4. Taking input - GFG/4.-taking-input.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<iostream>
+using namespace std;
+
+ void inputData();
+ 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+ void inputData()
+{
+       int a;
+       string b;
+       cin>>a>>b;
+       cout<<a<<" "<<b<<endl;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //function call
+        inputData();
+        
+    }
+}
+
+
+  // } Driver Code Ends
