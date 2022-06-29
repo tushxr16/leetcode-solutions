ba18eaea2896cea7e71563f256c9f80b8945c958
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-06-30 03:55:21 +0530

    Added solution - LeetHub

---
+++ 7. Learning to be precise - GFG/7.-learning-to-be-precise.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void precise(float a, float b)
+{
+    float ans = a/b;
+    
+    cout<<ans<<" "<<setprecision(3)<<fixed<<ans<<endl;
+    cout<<endl;
+}
+
+// { Driver Code Starts.
+void precise(float , float );//prototype
+
+//Driver Code
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    float a,b;
+	    cin>>a>>b; //Taking input of a and b in floating types
+	    precise(a,b);
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
