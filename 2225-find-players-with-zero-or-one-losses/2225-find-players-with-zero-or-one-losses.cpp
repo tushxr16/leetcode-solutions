8073ded7448dd97302dc18a43c62df33e0e24b0c
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-07-19 12:01:53 +0530

    Time: 1088 ms (51.36%), Space: 171.4 MB (55.13%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -2,30 +2,26 @@ class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         vector<vector<int>> ans;
-        map<int,int> mp;
+        map<int,int> cond1, cond2;
+        vector<int> tmp,tmp2;
         int n = matches.size();
         for(int i=0;i<n;i++){
-            mp[matches[i][0]]++;
+            cond1[matches[i][0]]++;
+            cond2[matches[i][1]]++;
         }
-        for(int i=0;i<n;i++){
-            if(mp.find(matches[i][1])!=mp.end())
-                mp.erase(matches[i][1]);
+        for(auto x:cond2){
+            if(cond1.find(x.first)!=cond1.end())
+                cond1.erase(x.first);
         }
-        vector<int> winners;
-        for(map<int,int>::iterator it = mp.begin(); it!=mp.end();it++){
-            winners.push_back((*it).first);
+        for(auto x:cond1){
+            tmp.push_back(x.first);
         }
-        ans.push_back(winners);
-        mp.clear();
-        for(int i=0;i<n;i++){
-            mp[matches[i][1]]++;
-        }
-        winners.clear();
-        for(auto x:mp){
+        ans.push_back(tmp);
+        for(auto x:cond2){
             if(x.second==1)
-                winners.push_back(x.first);
+                tmp2.push_back(x.first);
         }
-        ans.push_back(winners);
+        ans.push_back(tmp2);
         return ans;
     }
 };
