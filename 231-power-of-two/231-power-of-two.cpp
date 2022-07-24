e3743745535de00aa5193c6c5dfc4b1f6bf32a64
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-07-24 17:45:45 +0530

    Time: 7 ms (14.71%), Space: 5.9 MB (25.55%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int one = __builtin_popcount(abs(n));
+        int temp = n, one = 0;
+        while(temp){
+            one += (temp%2);
+            temp /=2;
+        }
         return one==1 && n>=0;
     }
 };
