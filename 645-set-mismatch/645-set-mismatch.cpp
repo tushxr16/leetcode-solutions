b91660f8d0e4697129d4a19087658126bc6f567f
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-09 00:02:54 +0530

    Time: 40 ms (80.82%), Space: 21.3 MB (86.89%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans = {1,1};
+        int n = nums.size();
+        int p = 1e4 + 1;
+        for(int i=0;i<n;i++){
+            nums[i]--;
+            int idx = nums[i]%p;
+            nums[idx] += p;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]/p==0)
+                ans[1] = i+1;
+            if(nums[i]/p==2)
+                ans[0] = i+1;
+        }
+        return ans;
+    }
+};
