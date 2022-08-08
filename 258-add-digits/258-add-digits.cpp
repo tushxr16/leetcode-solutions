35e3f4483aadeb2bbf38dc0ee65cc48fcd004c5e
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-08 13:05:16 +0530

    Time: 9 ms (7.28%), Space: 6 MB (25.14%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10){
+            return num;
+        }else{
+            int nextNum = 0;
+            while(num){
+                nextNum += num%10;
+                num /= 10;
+            }
+            return addDigits(nextNum);
+        }
+    }
+};
