db587f663fcb862e7400e5f0e30bbb23931e9beb
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-09 20:38:26 +0530

    Time: 286 ms (85.79%), Space: 110 MB (21.57%) - LeetHub

--- 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     int mod = 1e9 + 7;
     int totalStrength(vector<int>& s) {
-        long long res = 0, size = s.size();
-    vector<int> first1(size + 1), last1(size + 1),first2(size + 1), last2(size + 1),st; 
-    for (long long i = 0; i < size; i++) 
-        first1[i + 1] = (first1[i] + s[i]) % mod, last1[i + 1] = (last1[i] + (i + 1) * s[i]) % mod;
-    for (int i = size - 1; i >= 0; i--) 
-        first2[i] = (first2[i + 1] + s[i]) % mod, last2[i] = (last2[i + 1] + (size - i) * s[i]) % mod; 
-    for (int start = 0; start <= size; start++) {
-        while(!st.empty() && (start == size || s[st.back()] >= s[start])) {
-            int top_last = st.back();
-            st.pop_back();
-			long long left;
-			if(st.empty()) left = 0;
-			else left = st.back()+1;
-            long long sum_of_left = (mod + last1[top_last + 1] - last1[left] - left * (first1[top_last + 1] - first1[left]) % mod) % mod;
-            long long sum_of_right = (mod + last2[top_last + 1] - last2[start] - (size - start) * (first2[top_last + 1] - first2[start])) % mod;
-            long long sum_of_all = (sum_of_right * (top_last - left + 1) + sum_of_left * (start - top_last)) % mod;
-			res = (res+sum_of_all * s[top_last]) % mod;
+        long long res = 0, n = s.size();
+        vector<long long> pre1(n+1), pre2(n+1), suf1(n+1), suf2(n+1),st;
+        
+        for(long long i=0;i<n;i++){
+            pre1[i+1] = (pre1[i]+s[i])%mod;
+            suf1[i+1] = (suf1[i]+s[i]*(i+1))%mod;
         }
-        st.push_back(start);
-    }
-    return res;
+        for(long long i=n-1;i>=0;i--){
+            pre2[i] = (pre2[i+1]+s[i])%mod;
+            suf2[i] = (suf2[i+1]+s[i]*(n-i))%mod;
+        }
+        
+        for(long long i=0;i<=n;i++){
+            while(!st.empty() && (i==n || s[st.back()]>=s[i])){
+                long long top = st.back();
+                st.pop_back();
+                long long l = st.empty()?0:st.back()+1;
+                long long sl = (mod+suf1[top+1]-suf1[l]-l*(pre1[top+1]-pre1[l])%mod)%mod;
+                long long sr = (mod+suf2[top+1]-suf2[i]-(n-i)*(pre2[top+1]-pre2[i]))%mod;
+                long long sa = (sr*(top-l+1)+sl*(i-top))%mod;
+                res = (res+sa*s[top])%mod;
+            }
+            st.push_back(i);
+        }
+        return res;
     }
 };
