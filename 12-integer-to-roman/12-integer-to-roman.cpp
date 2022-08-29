de37b08d97b36cc3c973c85752afcab66665b243
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-29 09:06:09 +0530

    Time: 3 ms (96.83%), Space: 5.7 MB (99.08%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int intCode[] = {1000,900,500,400,100,
-                         90,50,40,10,9,5,4,1};
-        string strCode[] = {"M","CM","D","CD","C","XC","L",
-                             "XL","X","IX","V","IV","I"};
+        int intCode[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string stringCode[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         string ans;
-        
         for(int i=0;i<13;i++){
-            while(num >= intCode[i]){
-                ans += strCode[i];
+            while(intCode[i]<=num){
                 num -= intCode[i];
+                ans += stringCode[i];
             }
         }
-        
         return ans;
     }
 };
