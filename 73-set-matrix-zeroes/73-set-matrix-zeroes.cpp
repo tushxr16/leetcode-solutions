c54db213575f5a82db3e9b96eee35d32ff053176
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-30 23:38:52 +0530

    Time: 29 ms (28.32%), Space: 13.3 MB (53.53%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
         int n = matrix.size(), m = matrix[0].size();
-        vector<pair<int,int>> v;
+        vector<pair<int,int>> p;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    v.push_back({i,j});
+                    p.push_back({i,j});
                 }
             }
         }
-        for(auto p: v){
+        for(auto x: p){
+            int l = x.first;
+            int k = x.second;
             for(int i=0;i<n;i++){
-                matrix[i][p.second] = 0;
+                matrix[i][k] = 0;
             }
             for(int i=0;i<m;i++){
-                matrix[p.first][i] = 0;
+                matrix[l][i] = 0;
             }
         }
         return;
