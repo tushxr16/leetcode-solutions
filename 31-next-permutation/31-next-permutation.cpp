4af5e03f26cde62d41c857e74236e236f1c8f2d5
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-31 09:38:15 +0530

    Time: 12 ms (19.04%), Space: 12.2 MB (36.38%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int lastInc = -1, n = nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                lastInc = i;
+            }
+        }
+        if(lastInc==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int idx = lastInc;
+        for(int i=lastInc;i<n;i++){
+            if(nums[i]<nums[idx] && nums[i]>nums[lastInc-1]){
+                idx = i;
+            }
+        }
+        swap(nums[lastInc-1],nums[idx]);
+        sort(nums.begin()+lastInc,nums.end());
         return;
     }
 };
