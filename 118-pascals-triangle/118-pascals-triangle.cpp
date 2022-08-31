1d133b407eea1d64daa62410ef8a00b9bff1764a
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-08-31 08:53:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.34%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> v;
-        v.push_back({1});
-        if(n==1)
-            return v;
-        v.push_back({1,1});
-        for(int i=2;i<n;i++){
-            vector<int> tmp(i+1,1);
-            for(int j=0;j<(i+1)/2;j++){
-                tmp[j+1] = v[i-1][j]+v[i-1][j+1];
-                tmp[i-j-1] = tmp[j+1];
+        vector<vector<int>> pascalTriangle;
+        for(int i=0;i<n;i++){
+            pascalTriangle.push_back(vector<int>(i+1,1));
+            for(int j=1;j<i;j++){
+                pascalTriangle[i][j]=pascalTriangle[i-1][j-1]+pascalTriangle[i-1][j];
             }
-            v.push_back(tmp);
         }
-        return v;
+        return pascalTriangle;
     }
 };
