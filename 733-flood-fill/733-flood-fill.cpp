8aabfb6b3649d9b9a045de97805b007893424165
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-09-03 22:59:19 +0530

    Time: 12 ms (71.13%), Space: 14 MB (78.39%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
         queue<pair<int,int>> q;
         q.push({sr,sc});
-        int R = image.size(), C = image[0].size();
         int onColor = image[sr][sc];
-        
+        int r = image.size(), c = image[0].size();
+        if(color==onColor){
+            return image;
+        }
         while(!q.empty()){
             int n = q.size();
             for(int i=0;i<n;i++){
                 pair<int,int> p = q.front();
                 q.pop();
-                if(image[p.first][p.second]==onColor && image[p.first][p.second]!=color){
-                    image[p.first][p.second] = color;
-                    if(p.first+1<R){
-                        q.push({p.first+1,p.second});
-                    }
-                    if(p.first-1>=0){
-                        q.push({p.first-1,p.second});
-                    }
-                    if(p.second+1<C){
-                        q.push({p.first,p.second+1});
-                    }
-                    if(p.second-1>=0){
-                        q.push({p.first,p.second-1});
-                    }
+                image[p.first][p.second] = color;
+                if(p.first>0 && image[p.first-1][p.second]==onColor){
+                    q.push({p.first-1,p.second});
+                }
+                if(p.first<r-1 && image[p.first+1][p.second]==onColor){
+                    q.push({p.first+1,p.second});
+                }
+                if(p.second>0 && image[p.first][p.second-1]==onColor){
+                    q.push({p.first,p.second-1});
+                }
+                if(p.second<c-1 && image[p.first][p.second+1]==onColor){
+                    q.push({p.first,p.second+1});
                 }
             }
         }
