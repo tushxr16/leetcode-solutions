866672dca4d244b1bb2024d9b8517b61142f9557
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-09-04 11:29:37 +0530

    Time: 55 ms (14.61%), Space: 14.1 MB (50.80%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    bool dfs(int x, vector<bool> &vis, vector<bool> &dfsvis, vector<vector<int>> &graph){
-        vis[x] = true;
-        dfsvis[x] = true;
-        for(auto k: graph[x]){
-            if(!vis[k]){
-                if(dfs(k,vis,dfsvis,graph)){
-                    return true;
-                }
-            }else if(dfsvis[k]){
-                return true;
-            }
-        }
-        dfsvis[x] = false;
-        return false;
-    }
     bool canFinish(int V, vector<vector<int>>& prerequisites) {
-        vector<bool> vis(V,false);
-        vector<bool> dfsvis(V,false);
+        
+        vector<int> indeg(V,0);
         vector<vector<int>> graph(V);
+        queue<int> q;
+        int cnt = 1;
+        
         for(auto edge: prerequisites){
             graph[edge[0]].push_back(edge[1]);
+            indeg[edge[1]]++;
         }
+        
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,dfsvis,graph)){
-                    return false;
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
+            
+            for(auto x: graph[i]){
+                if(--indeg[x]==0){
+                    q.push(x);
+                    cnt++;
                 }
+            }
+        }
+        for(auto x: indeg){
+            if(x!=0){
+                return false;
             }
         }
         return true;
