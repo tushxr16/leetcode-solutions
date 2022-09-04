dcca75bbeb281740b035eb4d8316fac7813d0a75
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-09-04 11:09:03 +0530

    Time: 49 ms (22.23%), Space: 14.5 MB (31.75%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool dfs(int x, vector<bool> &dfsvis, vector<bool> &vis, vector<vector<int>> &adj){
+    bool dfs(int x, vector<bool> &vis, vector<bool> &dfsvis, vector<vector<int>> &graph){
         vis[x] = true;
         dfsvis[x] = true;
-        for(auto j: adj[x]){
-            if(!vis[j]){
-                if(dfs(j,dfsvis,vis,adj)){
+        for(auto k: graph[x]){
+            if(!vis[k]){
+                if(dfs(k,vis,dfsvis,graph)){
                     return true;
                 }
-            }else if(dfsvis[j]){
+            }else if(dfsvis[k]){
                 return true;
             }
         }
@@ -16,18 +16,15 @@ public:
         return false;
     }
     bool canFinish(int V, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(V);
-        
-        for(int i=0;i<prerequisites.size();i++){
-            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }
-        
         vector<bool> vis(V,false);
         vector<bool> dfsvis(V,false);
-        
+        vector<vector<int>> graph(V);
+        for(auto edge: prerequisites){
+            graph[edge[0]].push_back(edge[1]);
+        }
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(dfs(i,dfsvis,vis,graph)){
+                if(dfs(i,vis,dfsvis,graph)){
                     return false;
                 }
             }
