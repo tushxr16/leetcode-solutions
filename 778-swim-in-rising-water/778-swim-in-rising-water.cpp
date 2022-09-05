ec88e71fde02c88e87d5768d3ad624820399ee8e
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-09-05 18:28:02 +0530

    Time: 40 ms (50.07%), Space: 9.7 MB (54.49%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -2,54 +2,55 @@ class Solution {
 public:
     class Pair{
         public:
-            int row;
-            int col;
-            int msf;
+            int r;
+            int c;
+            int v;
 
-            Pair(int row, int col, int msf){
-                this->row = row;
-                this->col = col;
-                this->msf = msf;
+            Pair(int row, int col, int val){
+                this->r = row;
+                this->c = col;
+                this->v = val;
             }
     };
     
     class Compare{
         public:
-         bool operator()(Pair const &a, Pair const &b){
-            return a.msf > b.msf;
-        }
+            bool operator()(Pair const &a, Pair const &b){
+                return a.v > b.v;
+            }
     };
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
     
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         vector<vector<bool>> vis(n,vector<bool>(n,false));
         
-        priority_queue<Pair,vector<Pair>,Compare> pq;
+        priority_queue<Pair, vector<Pair>, Compare> pq;
         pq.push(Pair(0,0,grid[0][0]));
         
-        vis[0][0] = true;
-        
         while(!pq.empty()){
+            
             Pair top = pq.top();
             pq.pop();
+            vis[top.r][top.c] = true;
             
-            if(top.row==n-1 && top.col==n-1){
-                return top.msf;
+            if(top.r == n-1 && top.c == n-1){
+                return top.v;
             }
             
-            vis[top.row][top.col] = true;
-            
-            if(top.row+1<n && !vis[top.row+1][top.col])
-                pq.push(Pair(top.row+1,top.col,max(top.msf,grid[top.row+1][top.col])));
-            if(top.col+1<n && !vis[top.row][top.col+1])
-                pq.push(Pair(top.row,top.col+1,max(top.msf,grid[top.row][top.col+1])));
-            if(top.row-1>=0 && !vis[top.row-1][top.col])
-                pq.push(Pair(top.row-1,top.col,max(top.msf,grid[top.row-1][top.col])));
-            if(top.col-1>=0 && !vis[top.row][top.col-1])
-                pq.push(Pair(top.row,top.col-1,max(top.msf,grid[top.row][top.col-1])));
-            
+            for(int i=0;i<4;i++){
+                int nr = top.r + dx[i];
+                int nc = top.c + dy[i];
+                
+                if(nr<0 || nc<0 || nr>=n || nc>=n || vis[nr][nc]){
+                    continue;
+                }
+                
+                pq.push(Pair(nr,nc,max(top.v, grid[nr][nc])));
+                
+            }
         }
-        
         return 0;
     }
 };
