e945a045fa00c9ede19c0e9765f188ce12c3cd7c
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-09-05 10:36:33 +0530

    Time: 459 ms (49.80%), Space: 9.8 MB (88.68%) - LeetHub

--- 691-stickers-to-spell-word/691-stickers-to-spell-word.cpp
+++ 691-stickers-to-spell-word/691-stickers-to-spell-word.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int minStickers(vector<string>& stickers, string target){
-        int n = target.size(); 
-        vector<uint> dp(1 << n, -1); 
-        dp[0] = 0; 
-        for(int mask=0;mask<(1<<n);++mask)
-            if(dp[mask]!=-1)
-                for(string sticker: stickers){
-                    int mask0 = mask;
-                    for(char ch: sticker)
-                        for(int j=0;j<n;++j)
-                            if(ch==target[j]&&(mask0&(1<<j))==0){
-                                mask0 ^= 1<<j;
-                                break;
-                            }
-                    dp[mask0] = min(dp[mask0],1+dp[mask]);
+        int n = target.size();
+        vector<int> dp(1<<n,INT_MAX);
+        dp[0] = 0;
+        for(int i=0;i<1<<n;++i){
+            if(dp[i]!=INT_MAX)
+            for(string sticker: stickers){
+                int k = i;
+                for(char ch: sticker){
+                    for(int j=0;j<n;++j){
+                        if(ch==target[j] && (k&(1<<j))==0){
+                            k ^= 1<<j;
+                            break;
+                        }
+                    }
                 }
+                dp[k] = min(dp[k],dp[i]+1);
+            }
+        }
+        if(dp.back()==INT_MAX)
+            return -1;
         return dp.back();
     }
 };
