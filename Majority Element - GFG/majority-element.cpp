057eb5d4c133303e192b8d720f65913fac722bf4
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-12-06 13:54:25 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -1,11 +1,11 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 
 class Solution{
@@ -13,22 +13,35 @@ class Solution{
      // Function to find majority element in the array
     // a: input array
     // size: size of input array
-    int majorityElement(int a[], int size)
+    int majorityElement(int a[], int n)
     {
-        int cnt=0,maj_index=0;
-        for(int i=0;i<size;i++){
-            if(a[maj_index]==a[i])cnt++;
-            else cnt--;
-            if(cnt==0){maj_index=i;cnt++;}
+        int maj_index = 0, cnt = 0;
+        for(int i=0;i<n;i++){
+            if(a[maj_index]==a[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
+            if(cnt==0){
+                maj_index = i;
+                cnt++;
+            }
         }
-        cnt=0;
-        for(int i=0;i<size;i++)if(a[maj_index]==a[i])cnt++;
-        if(cnt>size/2)return a[maj_index];
-        else return -1;
+        cnt = 0;
+        for(int i=0;i<n;i++){
+            if(a[maj_index]==a[i]){
+                cnt++;
+            }
+        }
+        if(cnt>n/2){
+            return a[maj_index];
+        }else{
+            return -1;
+        }
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main(){
 
@@ -49,4 +62,5 @@ int main(){
 
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
