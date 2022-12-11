22186a6544a3873827872ee7c4edfb9a25566125
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-12-11 15:16:37 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -1,34 +1,47 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
   public:
-    long long int count(int S[], int m, int n) {
-        long long int dp[n+1]={0};
-        dp[0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=S[i];j<n+1;j++){
-                dp[j]+=dp[j-S[i]];
+    long long int count(int coins[], int N, int sum) {
+        
+        vector<vector<long long int>> dp(N+1,vector<long long int>(sum+1,0));
+        
+        for(int i=0;i<=N;i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=sum;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        return dp[n];
+        
+        return dp[N][sum];
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        int arr[m];
-        for (int i = 0; i < m; i++) cin >> arr[i];
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
         Solution ob;
-        cout << ob.count(arr, m, n) << endl;
+        cout << ob.count(coins, N, sum) << endl;
     }
 
     return 0;
-}  // } Driver Code Ends
+}
+
+
+// } Driver Code Ends
