82468671d56221f859e2c44c8b340e496d8b191b
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2023-08-18 14:25:34 +0530

    Added solution - LeetHub

---
+++ Expression Tree - GFG/expression-tree.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    string data;
+    struct node *left;
+    struct node *right;
+
+    node(string x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isExpression(string &s) {
+    if (s == "+" or s == "-" or s == "*" or s == "/") return true;
+    return false;
+}
+
+
+
+// } Driver Code Ends
+/*The structure of node is as follows
+struct node{
+    string data;
+    node *left;
+    node *right;
+};
+*/
+
+class Solution{
+  public:
+    /*You are required to complete below method */
+    int to_int(string s){
+        int ans = 0;
+        for(char c: s){
+            ans = ans*10 + (c-'0');
+        }
+        return ans;
+    }
+    int evalTree(node* root) {
+        if(!root) return 0;
+        if(root->left==nullptr && root->right==nullptr) return to_int(root->data);
+        int a = evalTree(root->left), b = evalTree(root->right);
+        if(root->data=="+") return a + b;
+        if(root->data=="*") return a * b;
+        if(root->data=="-") return a - b;
+        if(root->data=="/") return a / b;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s[n];
+        for (int i = 0; i < n; i++) cin >> s[i];
+
+        int p = 0;
+        node *root = new node(s[p++]);
+        queue<node *> q;
+        q.push(root);
+        while (!q.empty()) {
+            node *f = q.front();
+            q.pop();
+            node *l, *r;
+            if (isExpression(f->data)) {
+                l = new node(s[p++]);
+                r = new node(s[p++]);
+                f->left = l;
+                f->right = r;
+                q.push(l);
+                q.push(r);
+            }
+        }
+        Solution obj;
+        cout << obj.evalTree(root) << endl;
+    }
+}
+
+// } Driver Code Ends
