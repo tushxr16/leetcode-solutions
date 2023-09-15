c99f2dc7fd429bb7b6ab2e4043cee9a535296e96
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2023-09-15 12:40:49 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -1,41 +1,49 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
 public:
-    bool isBipartiteUtil(vector<int> adj[], vector<int> &color, int V, int x){
-        queue<int> q;
-        q.push(x);
-        color[x] = 1;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(auto x:adj[curr]){
-                if(color[x]==color[curr])
-                    return false;
-                if(color[x]==-1){
-                    color[x] = 1 - color[curr];
-                    q.push(x);
-                }
-            }
-        }
-        return true;
-    }
-	bool isBipartite(int V, vector<int>adj[]){
-	    vector<int> color(V+1,-1);
-	    for(int i=0;i<V;i++){
-	        if(color[i]==-1)
-	            if(!isBipartiteUtil(adj,color,V,i))
+	bool isBipartiteUtil(int k, int V, vector<int>adj[], vector<int> &color){
+
+	    queue<int> q;
+	    q.push(k);
+	    
+	    color[k] = 1;
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        for(int x: adj[node]){
+	            if(color[x]==-1){
+	                color[x] = 1 - color[node];
+	                q.push(x);
+	            }
+	            else if(color[x]==color[node]){
 	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+	
+	bool isBipartite(int V, vector<int> adj[]){
+	    vector<int> color(V,-1);
+	    
+	    for(int i=0;i<V;i++){
+	        if(color[i]==-1){
+	            if(!isBipartiteUtil(i,V,adj,color)){
+	                return false;
+	            }
+	        }
 	    }
 	    return true;
 	}
 
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main(){
 	int tc;
 	cin >> tc;
@@ -55,4 +63,5 @@ int main(){
 		else cout << "0\n";  
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
