455aa6371cd1f36b0577545aba8233738109244f
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2023-10-03 16:22:54 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -1,67 +1,54 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution 
 {
     public:
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(n==0)
-            return 0;
-        int m = grid[0].size();
-        queue<pair<int,int>> q;
+        queue<pair<int,pair<int,int> > > q;
+        int ans = 0, oranges = 0, n = grid.size(), m = grid[0].size();
         
-        int ans = 0;
-        
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
                 if(grid[i][j]==2)
-                    q.push({i,j});
-        }
-        
+                    q.push({0,{i,j}});
+                else if(grid[i][j]==1)
+                    oranges++;
+                    
         while(!q.empty()){
-            int q_n = q.size();
+            int timeSpent = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            q.pop();
             
-            for(int i=0;i<q_n;i++){
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-                if(x>0 && grid[x-1][y]==1){
-                    q.push({x-1,y});
-                    grid[x-1][y] = 2;
-                }
-                if(y>0 && grid[x][y-1]==1){
-                    q.push({x,y-1});
-                    grid[x][y-1] = 2;
-                }
-                if(x<n-1 && grid[x+1][y]==1){
-                    q.push({x+1,y});
-                    grid[x+1][y] = 2;
-                }
-                if(y<m-1 && grid[x][y+1]==1){
-                    q.push({x,y+1});
-                    grid[x][y+1] = 2;
+            ans = max(ans, timeSpent);
+            
+            int dx[4] = {0,1,0,-1};
+            int dy[4] = {1,0,-1,0};
+            
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx<n && nx>-1 && ny<m && ny>=-1 && grid[nx][ny]==1){
+                    oranges--;
+                    grid[nx][ny] = 2;
+                    q.push({timeSpent+1,{nx,ny}});
                 }
             }
-            if(q.size())
-                ans++;
+            
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1)
-                    return -1;
-            }
-        }
-        
-        return ans;
+        if(oranges!=0)
+            return -1;
+        else
+            return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main(){
 	int tc;
 	cin >> tc;
@@ -79,4 +66,5 @@ int main(){
 		cout << ans << "\n";
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
