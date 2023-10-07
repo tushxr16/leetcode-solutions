8284ae550e04520d6a1b8c41efe1dca89751bbbd
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2023-10-08 00:00:12 +0530

    Added solution - LeetHub

---
+++ Longest valid Parentheses - GFG/longest-valid-parentheses.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maxLength(string S){
+        int ans = 0, n = S.size();
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i=0;i<n;i++){
+            if(S[i]=='('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                    ans = max(ans, i-st.top());
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        
+        Solution ob;
+        cout<<ob.maxLength(S)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
